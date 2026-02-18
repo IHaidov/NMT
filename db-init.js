07c28c29826a2515cff61d6ae10b55660118b50a
ivan.haidov <ivan.haidov@shiphawk.com>
2026-02-18 21:37:48 +0100

    Don't add the db on deploy

--- db-init.js
+++ db-init.js
@@ -1,7 +1,8 @@
 const Database = require("better-sqlite3");
 const path = require("path");
 
-const dbPath = path.join(__dirname, "nmt.db");
+// На деплої (наприклад Render) вкажіть DATABASE_PATH на шлях у persistent volume, щоб БД не перезатиралась
+const dbPath = process.env.DATABASE_PATH || path.join(__dirname, "nmt.db");
 const db = new Database(dbPath);
 
 db.exec(`
